be75393f19ff8b540ec01860a8d6ab3dbc8548e6
Renato Ferreira <rfcbf@me.com>
2020-05-17 22:42:54 -0300

    Melhora na perfomace

---
+++ Movs/Controllers/Search/SearchCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchCollectionReusableView.swift
+//  Movs
+//
+//  Created by Renato Ferraz on 17/05/20.
+//  Copyright © 2020 Renato Ferraz. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+        
+}

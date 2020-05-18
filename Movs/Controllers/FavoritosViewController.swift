77462a254dd6cf4898da0eb294f1f99423751dcb
Renato Ferreira <rfcbf@me.com>
2020-05-17 21:18:45 -0300

    Acerto no lista de favoritos

--- Movs/Controllers/FavoritosViewController.swift
+++ Movs/Controllers/FavoritosViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class FavoritosViewController: UIViewController {
-
+    
     var favoritos : Array<Filmes> = []
     @IBOutlet weak var tableView: UITableView!
     var refreshControl = UIRefreshControl()
@@ -31,38 +31,56 @@ class FavoritosViewController: UIViewController {
         favoritos.sort {$0.title < $1.title}
         tableView.reloadData()
     }
-   
+    
     @objc func refresh(sender: AnyObject) {
-       recuperarDados()
-       refreshControl.endRefreshing()
+        recuperarDados()
+        refreshControl.endRefreshing()
     }
 }
 
 extension FavoritosViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return favoritos.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        
+        var numOfSections: Int = 1
+        if favoritos.count > 0
+        {
+            tableView.separatorStyle = .singleLine
+            numOfSections            = 1
+            tableView.backgroundView = nil
+        }
+        else
+        {
+            let noDataLabel: UILabel  = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+            noDataLabel.text          = "Sem favoritos"
+            noDataLabel.textAlignment = .center
+            tableView.backgroundView  = noDataLabel
+            tableView.separatorStyle  = .none
+        }
+        return numOfSections
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let urlImagem = URL(string: "\(Constantes.URL_IMAGEM)\(favoritos[indexPath.row].poster)")
-
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FavoritosTableViewCell
-
+        
         cell.txtTitle.text = favoritos[indexPath.row].title
         cell.txtAno.text = favoritos[indexPath.row].ano
         cell.txtDescricao.text = favoritos[indexPath.row].descricao
         cell.imagem.sd_setImage(with: urlImagem, placeholderImage: UIImage(named: "placeholder.png"), options: [.refreshCached, .progressiveLoad])
-
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
         
         let filmeSelecionado : Filmes = favoritos[indexPath.row]
         let vc = UIStoryboard.init(name: "Detail", bundle: Bundle.main).instantiateViewController(withIdentifier: "detalhes") as? DetailViewController
@@ -73,25 +91,25 @@ extension FavoritosViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
     {
-         return true
+        return true
     }
-        
+    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let contextItem = UIContextualAction(style: .destructive, title: "Desfavoritar") {  (contextualAction, view, boolValue) in
-
-            self.tableView.beginUpdates()
+            
+//            self.tableView.beginUpdates()
             let filmeSelecionado : Filmes = self.favoritos[indexPath.row]
-           
-           //remover do banco
-           Favoritos.apagarFavoritos(filme: filmeSelecionado)
-           //remove o registro do array
-           self.favoritos.remove(at: indexPath.row)
-           //remove da tabela
-           self.tableView.deleteRows(at: [indexPath], with: .automatic)
-           self.tableView.endUpdates()
+            
+            //remover do banco
+            Favoritos.apagarFavoritos(filme: filmeSelecionado)
+            //remove o registro do array
+            self.favoritos.remove(at: indexPath.row)
+            //remove da tabela
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            self.tableView.reloadData()
         }
         let swipeActions = UISwipeActionsConfiguration(actions: [contextItem])
-
+        swipeActions.performsFirstActionWithFullSwipe = true
         return swipeActions
     }
     
